20b3049fab9221210aebb444e54de35764f58014
Fashakin Olashubomi <subomifasakin@outlook.com>
2025-08-30 21:29:29 +0100

    refactor

--- web/routes/index.js
+++ web/routes/index.js
@@ -1,20 +1,18 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Nimble-ICE' })
-})
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Nimble-ICE" });
+});
 
 /* GET ice-flakes resource */
-router.get('/ice-flakes', function (req, res, next) {
-  res
-    .status(201)
-    .json({
-      resource: 'ice-flakes',
-      count: 1005,
-      shape: 'rectangle'
-    })
-})
+router.get("/ice-flakes", function (req, res, next) {
+  res.status(201).json({
+    resource: "ice-flakesss",
+    count: 1005,
+    shape: "rectangle",
+  });
+});
 
-module.exports = router
+module.exports = router;
